f588ddc38af4ca5232fa2e3db2c84931347962f0
Alex Ozdemir <aozdemir@hmc.edu>
2021-05-19 13:36:53 -0700

    Base of context, expr, sort, and solver classes (#1)
    Base of context, expr, sort, and solver classes
    
    Co-authored-by: makaimann <makaim@stanford.edu>
    Co-authored-by: Mathias Preiner <mathias.preiner@gmail.com>

---
+++ cvc5_z3py_compat/z3.py
@@ -0,0 +1,214 @@
+############################################
+# Copyright (c) 2021 The cvc5 Developers
+#               2012 The Microsoft Corporation
+#
+# cvc5's Z3-compatible Python interface
+#
+# Author: Alex Ozdemir (aozdemir)
+# pyz3 Author: Leonardo de Moura (leonardo)
+############################################
+
+"""
+cvc5 is an SMT solver.
+
+This is its (as much as possible) Z3-compatible python interface.
+
+Several online tutorials for Z3Py are available at:
+http://rise4fun.com/Z3Py/tutorial/guide
+
+Please send feedback, comments and/or corrections on the Issue tracker for
+https://github.com/cvc5/cvc5.git. Your comments are very valuable.
+
+TODO: example
+
+"""
+
+from .z3printer import *
+from fractions import Fraction
+from decimal import Decimal
+import decimal
+import sys
+import io
+import math
+import copy
+import functools as ft
+import operator as op
+
+import pycvc5 as pc
+from pycvc5 import kinds
+
+DEBUG = __debug__
+
+
+def debugging():
+    global DEBUG
+    return DEBUG
+
+
+def _is_int(v):
+    """ Python 2/3 agnostic int testing """
+    if sys.version < "3":
+        return isinstance(v, (int, long))
+    else:
+        return isinstance(v, int)
+
+
+class Context(object):
+    """A Context manages all terms, configurations, etc.
+
+    In cvc5's API, these are managed by a solver, but we keep the Context class
+    (which just wraps a solver) for compatiblity.
+
+    It's only responsibilities are:
+        * making fresh identifiers for a given sort
+        * looking up previously defined constants
+    """
+
+    def __init__(self, *args, **kws):
+        self.solver = pc.Solver()
+        self.solver.setOption("produce-models", "true")
+        # Map from (name, sort) pairs to constant terms
+        self.vars = {}
+        # An increasing identifier used to make fresh identifiers
+        self.next_fresh_var = 0
+
+    def __del__(self):
+        self.solver = None
+
+    def get_var(self, name, sort):
+        """Get the variable identified by `name`.
+
+        If no variable of that name (with that sort) has been created, creates
+        one.
+
+        Returns a Term
+        """
+        if (name, sort) not in self.vars:
+            self.vars[(name, sort)] = self.solver.mkConst(sort.ast, name)
+        return self.vars[(name, sort)]
+
+    def next_fresh(self, sort, prefix):
+        """Make a name such that (name, sort) is fresh.
+
+        The name will be prefixed by `prefix`"""
+        name = "{}{}".format(prefix, self.next_fresh_var)
+        while (name, sort) in self.vars:
+            self.next_fresh_var += 1
+            name = "{}{}".format(prefix, self.next_fresh_var)
+        return name
+
+    def __eq__(self, o):
+        return self.solver is o.solver
+
+
+# Global SMT context
+_main_ctx = None
+
+
+def main_ctx():
+    """Return a reference to the global context.
+
+    >>> x = Real('x')
+    >>> x.ctx == main_ctx()
+    True
+    >>> c = Context()
+    >>> c == main_ctx()
+    False
+    >>> x2 = Real('x', c)
+    >>> x2.ctx == c
+    True
+    >>> eq(x, x2)
+    False
+    """
+    global _main_ctx
+    if _main_ctx is None:
+        _main_ctx = Context()
+    return _main_ctx
+
+
+def _get_ctx(ctx):
+    if ctx is None:
+        return main_ctx()
+    else:
+        return ctx
+
+
+def get_ctx(ctx):
+    return _get_ctx(ctx)
+
+
+#########################################
+#
+# Term base class
+#
+#########################################
+
+
+class ExprRef(object):
+    """Constraints, formulas and terms are expressions."""
+
+    def __init__(self, ast, ctx=None, reverse_children=False):
+        self.ast = ast
+        self.ctx = _get_ctx(ctx)
+        self.reverse_children = reverse_children
+        assert isinstance(self.ast, pc.Term)
+        assert isinstance(self.ctx, Context)
+
+    def __del__(self):
+        if self.ctx is not None and self.ast is not None:
+            self.ctx = None
+            self.ast = None
+
+
+#########################################
+#
+# Sorts
+#
+#########################################
+
+
+class SortRef(object):
+    """A Sort is essentially a type. Every term has a sort"""
+
+    def __init__(self, ast, ctx=None):
+        self.ast = ast
+        self.ctx = _get_ctx(ctx)
+        assert isinstance(self.ast, pc.Sort)
+        assert isinstance(self.ctx, Context)
+
+    def __del__(self):
+        if self.ctx is not None:
+            self.ctx = None
+        if self.ast is not None:
+            self.ast = None
+
+
+#########################################
+#
+# Solver
+#
+#########################################
+
+
+class Solver(object):
+    """Solver API provides methods for implementing the main SMT 2.0 commands:
+    * push,
+    * pop,
+    * check,
+    * get-model,
+    * etc."""
+
+    def __init__(self, solver=None, ctx=None, logFile=None):
+        assert solver is None or ctx is not None
+        self.ctx = _get_ctx(ctx)
+        self.solver = self.ctx.solver
+        self.scopes = 0
+        self.assertions_ = [[]]
+        self.last_result = None
+        self.reset()
+
+    def __del__(self):
+        if self.solver is not None:
+            self.solver = None
+        if self.ctx is not None:
+            self.ctx = None
