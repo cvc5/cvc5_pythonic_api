1d0a4f22d8a0ca97b28776f69c7cb52ed92bfd2f
Alex Ozdemir <aozdemir@hmc.edu>
2021-06-06 16:19:35 -0700

    Add list casting as well.

--- cvc5_z3py_compat/z3.py
+++ cvc5_z3py_compat/z3.py
@@ -766,6 +766,25 @@ def _coerce_exprs(a, b, ctx=None):
     return (a, b)
 
 
+def _reduce(f, li, a):
+    r = a
+    for e in li:
+        r = f(r, e)
+    return r
+
+
+def _coerce_expr_list(alist, ctx=None):
+    has_expr = False
+    for a in alist:
+        if is_expr(a):
+            has_expr = True
+            break
+    if not has_expr:
+        alist = [_py2expr(a, ctx) for a in alist]
+    s = _reduce(_coerce_expr_merge, alist, None)
+    return [s.cast(a) for a in alist]
+
+
 def is_expr(a):
     """Return `True` if `a` is an SMT expression.
 
