c4dbfa885a0a77dd8aed3989994f3b3ef3b5f72e
Gereon Kremer <gkremer@stanford.edu>
2021-11-30 14:39:28 -0800

    Apply suggestions from code review
    Co-authored-by: Alex Ozdemir <aozdemir@hmc.edu>

--- test/pgms/example_linear_arith.py
+++ test/pgms/example_linear_arith.py
@@ -5,11 +5,11 @@ slv = SolverFor('QF_LIRA')
 x = Int('x')
 y = Real('y')
 
-slv.add(And(x >= 3 * y, x <= y, -2 < x))
+slv += And(x >= 3 * y, x <= y, -2 < x)
 slv.push()
 print(slv.check(y-x <= 2/3))
 slv.pop()
 slv.push()
-slv.add(y-x == 2/3)
+slv += y-x == 2/3
 print(slv.check())
 slv.pop()
