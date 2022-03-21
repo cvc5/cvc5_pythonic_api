fe587789e38b21fcd528a57c8e386807d21a7afa
Alex Ozdemir <aozdemir@hmc.edu>
2022-03-21 16:47:06 -0700

    Non-zero exit on doctest failure

--- z3doc_test.py
+++ z3doc_test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
+
 if __name__ == "__main__":
     import doctest
     import cvc5_z3py_compat
 
-    doctest.testmod(cvc5_z3py_compat.z3)
+    n_failed, _ = doctest.testmod(cvc5_z3py_compat.z3)
+    if n_failed > 0:
+        sys.exit(1)
