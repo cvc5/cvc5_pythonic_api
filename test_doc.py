bb05fe1fa37863c6bbb151820a20868d33cd33e1
Gereon Kremer <gkremer@cs.stanford.edu>
2022-04-01 13:03:54 -0700

    fix

--- test_doc.py
+++ test_doc.py
@@ -4,8 +4,8 @@ import sys
 
 if __name__ == "__main__":
     import doctest
-    import cvc5_z3py_compat
+    import cvc5_pythonic_api
 
-    n_failed, _ = doctest.testmod(cvc5_z3py_compat.z3)
+    n_failed, _ = doctest.testmod(cvc5_pythonic_api.cvc5_pythonic)
     if n_failed > 0:
         sys.exit(1)
