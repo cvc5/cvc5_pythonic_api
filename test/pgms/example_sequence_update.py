a1f14f80363e79e4e3568af3d0841f5da163269e
Alex Ozdemir <aozdemir@hmc.edu>
2023-08-08 14:13:56 -0700

    stabilize seq tests
    previously, they were non-det.

--- test/pgms/example_sequence_update.py
+++ test/pgms/example_sequence_update.py
@@ -1,6 +1,13 @@
 from cvc5_pythonic_api import *
-s,t = Consts('s t',SeqSort(IntSort()))
+
+
+def mysolve(*args):
+    s = Solver()
+    print(s.check(*args))
+
+
+s, t = Consts("s t", SeqSort(IntSort()))
 print(is_seq(s))
-solve(SeqUpdate(s,t,0) == Concat(Unit(IntVal(1)),Unit(IntVal(2))))
-a = String('a')
-solve(SeqUpdate(a,StringVal('llo'),2) == 'hello')
+mysolve(SeqUpdate(s, t, 0) == Concat(Unit(IntVal(1)), Unit(IntVal(2))))
+a = String("a")
+mysolve(SeqUpdate(a, StringVal("llo"), 2) == "hello")
